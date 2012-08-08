8aab83884e5f9cf8340565ecc64b641c6a6e23b7
Platon Dickey <pdickey@ltutech.com>
2012-08-08 19:38:33 -0400

    brought up to date

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,7 +1,37 @@
-var App = Em.Application.create({
-  title: '',
-  ready: function() {
-    document.title = this.get('title');
-    $('.page-header .title').html(this.get('title'));
-  }
+
+var App = Em.Application.create({});
+
+
+/********
+ * Data *
+ ********/
+
+App.store = DS.Store.create({
+  revision: 4,
+  adapter: DS.FixtureAdapter.create()
 });
+
+
+/***********************
+ * Controllers & Views *
+ ***********************/
+
+App.ApplicationController = Em.Controller.extend({});
+App.ApplicationView = Em.View.extend({
+  templateName: 'application'
+});
+
+
+/**********
+ * Router *
+ **********/
+
+App.Router = Em.Router.extend({
+  root: Em.Route.extend({
+    index: Em.Route.extend({
+      route: '/'
+    })
+  })
+});
+
+App.initialize();
